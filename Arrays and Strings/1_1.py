6efefecd0cf4a19314fca5600f14914329487ccf
LO-2002-2024 <123707233+lukeolsen06@users.noreply.github.com>
2025-10-27 11:28:28 -0400

    Adding comments to 1_1.py

--- Arrays and Strings/1_1.py
+++ Arrays and Strings/1_1.py
@@ -1,15 +1,21 @@
 
+# Question 1.1: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 
-def isUnique_bitVector(s):
+# Solution using bit vector (assuming only lowercase 'a' to 'z')
+def isUnique_bitVector(string):
     checker = 0
-    for c in s:
-        val = ord(c) - ord('a')
+    for c in string:
+        val = ord(c) - ord('a')  # Create an integer value for each character in the string
         mask  = 1 << val
-        if (checker & mask) > 0:
+        if (checker & mask) > 0: # Perform bitwise AND. If the bit is already set, character is not unique
             return False
-        checker |= mask
+        checker |= mask          # Set the bit for this character
     return True
     
-    
+
+# Test cases  
 print(isUnique_bitVector("abcdefg"))  # True
 print(isUnique_bitVector("pizza"))  # False
+
+
+# Algorithm runs in O(n) time and uses O(1) space.
