f2866e28ddd899d0172cda5a0f797010c2554df8
LO-2002-2024 <123707233+lukeolsen06@users.noreply.github.com>
2025-11-04 12:29:08 -0500

    1.9

---
+++ Arrays and Strings/1_9.py
@@ -0,0 +1,19 @@
+# Question 1.9: String Rotation: Assume you have a method isSubstring which checks if one word is a substring of another.
+# Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring 
+# (e.g., "waterbottle" is a rotation of "erbottlewat").
+
+# Runtime: O(n) where n is the length of the strings
+# Space: O(n) where n is the length of the strings
+
+def string_rotation(s1, s2):
+    if len(s1) != len(s2):
+        return False
+    s1s1 = s1 + s1
+    return isSubstring(s1s1, s2)
+
+# Example implementation of isSubstring
+def isSubstring(s1, s2):
+    return s2 in s1
+
+print(string_rotation("waterbottle", "erbottlewat"))
+print(string_rotation("evermore", "ermoreev"))
